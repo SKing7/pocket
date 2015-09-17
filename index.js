0c9ee8d245da00c00d5a744f2dd2e79a14c59d68
zhe.liu <sking.pt@gmail.com>
2015-09-17 12:04:37 +0800

    修改目录

---
+++ index.js
@@ -0,0 +1,36 @@
+~(function () {
+    "use strict";
+    const pocketHome = process.cwd();
+    const path = require('path');
+    const https = require('https');
+    const qs = require('querystring');
+
+    const fs = require('fs');
+    const readability = require('node-readability');
+    const exec = require('child_process').exec;
+
+    const $util = require('./lib/util');
+
+    const postData = require('./lib/accessInfo'); 
+    const compose = require('./lib/composite'); 
+
+
+    let content = qs.stringify(postData);
+    let options = $util.getConfig(content);
+    var req = https.request(options, function(res) {
+        var dataList = '';
+        res.on('data', function(chunk) {
+            dataList += chunk;
+        });
+        res.on('end', function(e) {
+            console.log('request end');
+            compose(dataList);
+            //fs.writeFile(pocketHome + 'json_tpl', dataList, function () {});
+        })
+    });
+    req.on('error', function(e) {
+        console.log('Error got: ' + e.message);
+    });
+    req.write(content);
+    req.end();
+}())
